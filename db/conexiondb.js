587cdde63df67db6c95a2fef033240bb19a79954
Ismael Mendoza <ismaelmendoza768@gmail.com>
2022-07-17 13:50:50 -0500

    actualizando parametros

--- db/conexiondb.js
+++ db/conexiondb.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool
 
 
 const dbParams = {
-    user:"zcgyexkeswijsc",
-    host:"ec2-34-239-241-121.compute-1.amazonaws.com",
-    database:"dcjhcp6nb4003h",
-    password:"6065823d6595ae2bb7e679074972cd7e2ca81a5ae3179da2cf67e868b51dc237",
+    user:"qxibmjic",
+    host:"heffalump.db.elephantsql.com (heffalump-01)",
+    database:"qxibmjic",
+    password:"xt4k5HNR1y8Pb-bU-6bagbkfEhfb9UH_",
     port:5432,
     ssl: {rejectUnautorized:false}
 }
