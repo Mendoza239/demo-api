ddff5596dd0c91e29d3a3cf43e424ba958f8bf12
Ismael Mendoza <ismaelmendoza768@gmail.com>
2022-07-18 09:27:58 -0500

    estableciendo conexion

--- db/conexion.js
+++ db/conexion.js
@@ -1,11 +1,10 @@
 const Pool = require('pg').Pool
 
 const dbParams = {
-    user: 'zcgyexkeswijsc',
-    host: 'ec2-34-239-241-121.compute-1.amazonaws.com',
-    database: 'dcjhcp6nb4003h',
-    password: '6065823d6595ae2bb7e679074972cd7e2ca81a5ae3179da2cf67e868b51dc237',
-    port:5432,
+    user: 'ofxfctfq',
+    host: 'heffalump.db.elephantsql.com',
+    database: 'ofxfctfq',
+    password: 'hzrm1pn7s0p8AKs7z45JGweEGfdyeBZN',
     ssl: { rejectUnauthorized: false }
 }
 
@@ -14,7 +13,6 @@ const pool = new Pool({
     host: dbParams.host,
     database: dbParams.database,
     password: dbParams.password,
-    port: dbParams.port,
     ssl: { rejectUnauthorized: false }
 });
 
